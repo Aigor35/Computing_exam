a821968fa8161555e48c0bacb5650548d5fea29c
Aigor35 <alexcolavecchia1356@gmail.com>
2021-06-28 11:56:19 +0200

    I added functions in order to calculate focal length of the camera and distance between user and camera. I've started to implement also face detection in order to refine the tracking of the user and to supply the functions mentioned above with the right parameters.

--- Virtual_face_motion.py
+++ Virtual_face_motion.py
@@ -8,22 +8,17 @@ import numpy
 
 
 
-face = Mesh('Models/STL_Head.stl').rotateX(-90).rotateY(180)
+faceMesh = Mesh('Models/STL_Head.stl').rotateX(-90).rotateY(180)
+# Width of my face in real world (measured)
+referenceFaceWidth = 14 #cm
+# Distance from the camera in the reference image (measured)
+referenceDistance = 53 #cm
+faceCascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 
 
-cap = cv.VideoCapture(0)
 
-tracker = cv.legacy.TrackerCSRT_create()
 
-success, img = cap.read()
-boundingBox = cv.selectROI("Tracking", img, False)
-tracker.init(img, boundingBox)
-
-plotter_1 = Plotter(axes=dict(xtitle='x axis', ytitle='y axis', ztitle='z axis', yzGrid=False),
-                    size=(img.shape[1], img.shape[0]),)
-
-vedo.show(face, axes=1)
 
 
 def drawBox(img, boundingBox):
@@ -32,11 +27,59 @@ def drawBox(img, boundingBox):
     cv.putText(img, "Tracking", (75, 75), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
 
+# I need to add a way to select only the biggest face detected.
+def getFaceData(img):
+    faceWidth = 0
+    imgGrey = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    facesDetected = faceCascade.detectMultiScale(imgGrey,1.3,5)
+    for (x,y,w,h) in facesDetected:
+        cv.rectangle(img,(x,y),(x+w,y+h),(0,0,255),1)
+        faceWidth = w
+    return faceWidth
+
+
+def getFocalLength(referenceDistance,referenceFaceWidth,faceWidthInFrame):
+    focalLength = (faceWidthInFrame*referenceDistance)/referenceFaceWidth
+    return focalLength
+
+
+def getDistance(focalLength,referenceFaceWidth,faceWidthInFrame):
+    distance = (referenceFaceWidth*focalLength)/faceWidthInFrame
+    return distance
+
 
 def moveFace(img,boundingBoxOld,boundingBoxNew,face):
     x_old, y_old, w_old, h_old = int(boundingBoxOld[0]), int(boundingBoxOld[1]), int(boundingBoxOld[2]), int(boundingBoxOld[3])
     x_new, y_new, w_new, h_new = int(boundingBoxNew[0]), int(boundingBoxNew[1]), int(boundingBoxNew[2]), int(boundingBoxNew[3])
     face.addPos(x_new-x_old,-y_new+y_old,0)
+
+
+
+
+
+cap = cv.VideoCapture(0)
+referenceImage = cv.imread('Models/Reference_image.png')
+tracker = cv.legacy.TrackerCSRT_create()
+
+
+success, img = cap.read()
+referenceImage = cv.resize(referenceImage,(img.shape[1],img.shape[0]))
+
+referenceImageFaceWidth = getFaceData(referenceImage)
+focalLength = getFocalLength(referenceDistance,referenceFaceWidth,referenceImageFaceWidth)
+
+boundingBox = cv.selectROI("Tracking", img, False)
+tracker.init(img, boundingBox)
+
+plotter_1 = Plotter(axes=dict(xtitle='x axis', ytitle='y axis', ztitle='z axis', yzGrid=False),
+                    size=(img.shape[1], img.shape[0]),)
+vedo.show(faceMesh, axes=1)
+
+
+
+
+
+
 
 
 while True:
@@ -48,20 +91,57 @@ while True:
     if success:
         boundingBoxNew = deepcopy(boundingBox)
         drawBox(img, boundingBox)
-        moveFace(img,boundingBoxOld,boundingBoxNew,face)
+        moveFace(img,boundingBoxOld,boundingBoxNew,faceMesh)
     else:
         cv.putText(img, "Object lost", (75, 75), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
     fps = cv.getTickFrequency() / (cv.getTickCount() - timer)
 
+    faceWidth = getFaceData(img)
+    distance = getDistance(focalLength,referenceFaceWidth,faceWidth)
     cv.putText(img, str(int(fps)), (75, 50), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     cv.imshow("Tracking", img)
-    vedo.show(face, axes=1)
-    print(face.pos())
+    vedo.show(faceMesh, axes=1)
 
     if cv.waitKey(1) & 0xff == ord('q'):
         break
 
+
+"""
+while True:
+    success,img = cap.read()
+    faceWidth = getFaceData(img)
+    cv.imshow("frame",img)
+    if cv.waitKey(1) & 0xff == ord('q'):
+        break
+"""
+
 vedo.plotter.closePlotter()
 vedo.closeWindow()
+cap.release()
 cv.destroyAllWindows()
+
+"""
+ret,frame1 = cv.VideoCapture(0)
+ret,frame2 = cv.VideoCapture(0)
+diff = cv.absdiff(frame1,frame2)
+gray = cv.cvtColor(diff,cv.COLOR_BGR2GRAY)
+blur = cv.GaussianBlur(gray,(5,5),0)
+_ , thresh = cv.threshold(blur,20,255,cv.THRESH_BINARY)
+dilated = cv.dilate(thresh,None,iterations=3)
+contours, _ = cv.findContours(dilated,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+cv.drawContours(frame1,contours,-1,(0,255,0),2)
+frame1 = frame2
+ret,frame2 = cap.read()
+# Up until now the program draws the contours of every moving object, not bounding boxes.
+for contour in contours:
+    (x,y,w,h) = cv.boundingRect(contour)
+    if cv.contourArea(contour) < 700:
+        continue
+    cv.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
+    
+    
+
+"""
+
+

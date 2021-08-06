2d1d44bbe4499b47ecec6d26d5fe705cafdeda42
Aigor35 <alexcolavecchia1356@gmail.com>
2021-08-06 10:14:58 +0200

    The program Anchoring.py is now able to track down multiple points selected by the user.

--- Anchoring.py
+++ Anchoring.py
@@ -2,17 +2,16 @@ import numpy as np
 import cv2 as cv
 
 
-def selectPoint(event,x,y,flags,params):
+def selectPoints(event,x,y,flags,params):
     global point, pointSelected, oldPoints
     if event == cv.EVENT_LBUTTONDOWN:
         point = (x,y)
         pointSelected = True
-        oldPoints = np.array([[x, y]],dtype=np.float32)
+        oldPoints = np.append(oldPoints,[[np.float32(x),np.float32(y)]])
+        oldPoints = np.reshape(oldPoints, (int(len(oldPoints.T)/2), 2))
 
 
 cap = cv.VideoCapture(0)
-anchors = []
-
 
 
 lkParams = dict(winSize = (10,10),
@@ -22,13 +21,13 @@ lkParams = dict(winSize = (10,10),
 pointSelected = False
 point = ()
 
-oldPoints = np.array([[]])
+oldPoints = np.array([[]],dtype=np.float32)
 
 _, oldFrame = cap.read()
 oldFrameGray = cv.cvtColor(oldFrame,cv.COLOR_BGR2GRAY)
 
 cv.namedWindow("Frame")
-cv.setMouseCallback("Frame",selectPoint)
+cv.setMouseCallback("Frame",selectPoints)
 
 
 while True:
@@ -37,14 +36,12 @@ while True:
     newFrameGray = cv.cvtColor(newFrame,cv.COLOR_BGR2GRAY)
 
     if pointSelected == True:
-        cv.circle(newFrame,point,5,(0,0,255),-1)
         newPoints, status, error = cv.calcOpticalFlowPyrLK(oldFrameGray,newFrameGray,oldPoints,None,**lkParams)
         oldPoints = newPoints
         oldFrameGray = newFrameGray.copy()
 
-
-        x, y = newPoints.ravel()
-        cv.circle(newFrame, (int(x), int(y)), 5, (0, 255, 0), -1)
+        for point in oldPoints:
+            cv.circle(newFrame, (int(point[0]), int(point[1])), 5, (0, 255, 0), -1)
 
     cv.imshow("Frame", newFrame)
 

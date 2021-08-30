dd2a80acc0205ba4a4f34445d10985ee63ae735a
Aigor35 <alexcolavecchia1356@gmail.com>
2021-08-30 18:44:31 +0200

    Added more unit tests related to the particular cases of the function getFocalLength.

--- Tests.py
+++ Tests.py
@@ -1,9 +1,12 @@
+import hypothesis.extra.numpy
 import pytest
 from hypothesis import given
 from hypothesis import strategies as st
 from hypothesis import example
 from hypothesis import assume
+from hypothesis import settings
 import Virtual_face_motion as vfm
+import numpy as np
 
 
 
@@ -26,6 +29,107 @@ def test_getFocalLength_giveRefAreaEqualToZero_raiseExeption():
         vfm.getFocalLength(refDistance, refArea, refDetectedArea)
 
 
+def test_getFocalLength_giveRefDistanceLowerThanZero_raiseExeption():
+    refDistance = -1
+    refArea = 1
+    refDetectedArea = 1
+    with pytest.raises(ValueError):
+        vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefAreaLowerThanZero_raiseExeption():
+    refDistance = 1
+    refArea = -1
+    refDetectedArea = 1
+    with pytest.raises(ValueError):
+        vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefDetectedAreaLowerThanZero_raiseExeption():
+    refDistance = 1
+    refArea = 1
+    refDetectedArea = -1
+    with pytest.raises(ValueError):
+        vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefDistanceEqualToNan_raiseExeption():
+    refDistance = float("nan")
+    refArea = 1
+    refDetectedArea = 1
+    with pytest.raises(ValueError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+
+def test_getFocalLength_giveRefAreaEqualToNan_raiseExeption():
+    refDistance = 1
+    refArea = float("nan")
+    refDetectedArea = 1
+    with pytest.raises(ValueError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+
+def test_getFocalLength_giveRefDetectedAreaEqualToNan_raiseExeption():
+    refDistance = 1
+    refArea = 1
+    refDetectedArea = float("nan")
+    with pytest.raises(ValueError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefDistanceEqualToInf_raiseExeption():
+    refDistance = float("inf")
+    refArea = 1
+    refDetectedArea = 1
+    with pytest.raises(ValueError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefAreaEqualToInf_raiseExeption():
+    refDistance = 1
+    refArea = float("inf")
+    refDetectedArea = 1
+    with pytest.raises(ValueError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefDetectedAreaEqualToInf_raiseExeption():
+    refDistance = 1
+    refArea = 1
+    refDetectedArea = float("inf")
+    with pytest.raises(ValueError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefDistanceNotNumerical_raiseExeption():
+    refDistance = True
+    refArea = 1
+    refDetectedArea = 1
+    with pytest.raises(TypeError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+def test_getFocalLength_giveRefAreaNotNumerical_raiseExeption():
+    refDistance = 1
+    refArea = True
+    refDetectedArea = 1
+    with pytest.raises(TypeError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+
+def test_getFocalLength_giveRefDetectedAreaNotNumerical_raiseExeption():
+    refDistance = 1
+    refArea = 1
+    refDetectedArea = True
+    with pytest.raises(TypeError):
+        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
+
+
+
+
 def test_getFocalLength_giveSpecificValues_returnSpecificResult():
     refDistance = 1
     refArea = 4
@@ -34,19 +138,29 @@ def test_getFocalLength_giveSpecificValues_returnSpecificResult():
     assert focalLength == 2
 
 
-def test_getFocalLength_giveNegativeParameters_raiseExeption():
-    refDistance = 1
-    refArea = -4
-    refDetectedArea = 16
-    with pytest.raises(ValueError):
-        focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)
 
 
-@given(refDistance = st.floats(), refArea = st.floats(), refDetectedArea = st.floats())
-def test_hypothesis(refDistance, refArea, refDetectedArea):
-    assume(refDistance > 0)
-    assume(refArea > 0)
-    assume(refArea > 0)
-    assert vfm.getFocalLength(refDistance, refArea, refDetectedArea)**2 == (refDistance**2)*refDetectedArea/refArea
+#@settings(report_multiple_bugs= True)
+@given(refDistance = st.floats(min_value = 10**(-10), max_value = 10**5, allow_nan = False,
+                               allow_infinity = False,  exclude_min = True, exclude_max = True),
+       refArea = st.floats(min_value = 10**(-10), max_value = 10**5, allow_nan = False,
+                               allow_infinity = False,  exclude_min = True, exclude_max = True),
+       refDetectedArea = st.floats(min_value = 10**(-10), max_value = 10**5, allow_nan = False,
+                               allow_infinity = False,  exclude_min = True, exclude_max = True))
+def test_getFocalLength_propertyBasedTest(refDistance, refArea, refDetectedArea):
+    focalLength = vfm.getFocalLength(refDistance, refArea, refDetectedArea)**2
+    assert focalLength - (refDistance**2)*refDetectedArea/refArea < 10**(-5)
 
 
+
+"""
+@given(focalLength = st.floats(min_value = 10**(-10), max_value = 10**5, allow_nan = False,
+                               allow_infinity = False,  exclude_min = True, exclude_max = True),
+       refArea = st.floats(min_value=10 ** (-10), max_value=10 ** 5, allow_nan=False,
+                         allow_infinity = False, exclude_min = True, exclude_max = True),
+       boxPoints = hypothesis.extra.numpy.arrays(np.float32, shape = (4, 2),
+                                                 elements = st.floats(allow_nan = False, allow_infinity = False)))
+def test_getDistance_propertyBasedTest(focalLength,refArea,boxPoints):
+    distance = vfm.getDistance(focalLength,refArea,boxPoints)
+    assert distance - (focalLength**2)*refArea/
+"""

aead090b4cc4ae0b18b50386fea07c2bfa3defad
Aigor35 <alexcolavecchia1356@gmail.com>
2021-09-24 18:04:52 +0200

    I changed the function that rotates the virtual face so that it simply returns the detected rotation angle. The actual rotation is delegated to a function of the "vedo" library. The modified function has already been tested, and the tests can be found inside the Tests.py file. I also deleted the comments present inside the code and made it more readable.

--- Virtual_face_motion.py
+++ Virtual_face_motion.py
@@ -5,9 +5,6 @@ import numpy as np
 
 
 
-# TO DO: Aggiungere gestione bordi
-# TO DO: Migliorare l'acquisizione del primo punto
-# TO DO: Eliminare il print delle coordinate in moveFace
 
 
 
@@ -23,20 +20,8 @@ def manageTrackedPoints(event, x, y, flags, params):
         faceMesh = Mesh('Models/STL_Head.stl').rotateX(-90).rotateY(180)
 
 
-"""
-# refDistance and refLength are in cm, refDetectedLength is in pixels
-def getFocalLength(refDistance,refLength,refDetectedLength):
-    focalLength = refDistance*(refDetectedLength/refLength)
-    return focalLength
-
-# refLength is in cm, focalLength and detectedLength are in pixels
-def getDistance(focalLength,refLength,detectedLength):
-    distance = refLength*(focalLength/detectedLength)
-    return distance
-"""
 
 acceptedTypes = (int, float, np.float32, np.float64)
-
 
 def getFocalLength(refDistance, refArea, refDetectedArea):
     if (refDistance < 0 or refArea < 0 or refDetectedArea < 0):
@@ -52,6 +37,7 @@ def getFocalLength(refDistance, refArea, refDetectedArea):
     return refDistance*np.sqrt(refDetectedArea/refArea)
 
 
+
 def getCmOverPixelsRatio(refArea, boxPoints):
     if boxPoints.size == 0:
         raise ValueError("An empty array of points was passed to the function.")
@@ -64,7 +50,6 @@ def getCmOverPixelsRatio(refArea, boxPoints):
 
 
 
-# This returns np.float64
 def getDistance(focalLength, cmOverPixelsRatio):
     return focalLength*cmOverPixelsRatio
 
@@ -72,23 +57,22 @@ def getDistance(focalLength, cmOverPixelsRatio):
 
 def moveFace(oldPoints, newPoints, focalLength, refArea):
     global faceMesh
-    if (np.isnan(np.sum(newPoints)) or np.isinf(np.sum(newPoints))):
-        raise ValueError("The newly tracked points have Nan or infinite value.")
-    else:
-        oldCentroid = np.mean(oldPoints, axis=0)
-        newCentroid = np.mean(newPoints, axis=0)
-        newRatio = getCmOverPixelsRatio(refArea, newPoints)
+    oldCentroid = np.mean(oldPoints, axis=0)
+    newCentroid = np.mean(newPoints, axis=0)
+    newRatio = getCmOverPixelsRatio(refArea, newPoints)
 
-        oldDistance = getDistance(focalLength, getCmOverPixelsRatio(refArea, oldPoints))
-        newDistance = getDistance(focalLength, newRatio)
+    oldDistance = getDistance(focalLength, getCmOverPixelsRatio(refArea, oldPoints))
+    newDistance = getDistance(focalLength, newRatio)
 
-        faceMesh.addPos(int(newCentroid[0] - oldCentroid[0]) * newRatio,
-                        int(-newCentroid[1] + oldCentroid[1]) * newRatio,
-                        -newDistance + oldDistance)
-        # print(faceMesh.pos())
+    faceMesh.addPos(int(newCentroid[0] - oldCentroid[0]) * newRatio,
+                    int(-newCentroid[1] + oldCentroid[1]) * newRatio,
+                    -newDistance + oldDistance)
+
 
 
 def checkIfInsideBoundary(boxPoints, windowWidth, windowLength):
+    if (np.isnan(np.sum(boxPoints)) or np.isinf(np.sum(boxPoints))):
+        raise ValueError("The newly tracked points have some coordinates equal to Nan or infinity.")
     maxBoundary = np.full((4, 2), [windowWidth, windowLength])
     minBoundary = np.full((4,2), [0,0])
     if (np.any(boxPoints >= maxBoundary) or np.any(boxPoints <= minBoundary)):
@@ -98,24 +82,26 @@ def checkIfInsideBoundary(boxPoints, windowWidth, windowLength):
         pass
 
 
-# I think I can use faceMesh.rotateX().rotateY().rotateZ()
-# I'm not using radiants, so rad = False (it's the default value)
-def rotateFace(oldRectangle, newRectangle):
+
+def getRotationAngle(oldRectangle, newRectangle):
     global faceMesh
     detectedAngleDifference = -newRectangle[-1]+oldRectangle[-1]
     if abs(detectedAngleDifference) > 60:
         detectedAngleDifference = 0
-    faceMesh.rotateY(detectedAngleDifference,locally=True)
+    return detectedAngleDifference
 
 
-def printFacePosition(faceMesh):
+
+def showFacePosition(faceMesh):
     position = faceMesh.pos()
-    x = np.around(position[0],2)
-    y = np.around(position[1],2)
-    z = np.around(position[2],2)
+    x = np.around(position[0], 2)
+    y = np.around(position[1], 2)
+    z = np.around(position[2], 2)
     cv.putText(newFrame, "x: "+str(x), (75, 50), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     cv.putText(newFrame, "y: "+str(y), (75, 75), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     cv.putText(newFrame, "z: "+str(z), (75, 100), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
 
 
 
@@ -128,48 +114,34 @@ if __name__ == "__main__":
 
     cap = cv.VideoCapture(0)
 
-
-
     lkParams = dict(winSize = (10,10),
                     maxLevel = 4, # It's the pyramid level; each level denotes a window whose size is half of the previous one.
                     criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
-
-
 
     success, oldFrame = cap.read()
 
     if not success:
         raise ValueError("The system failed in reading the first frame.")
-    oldFrame = cv.flip(oldFrame,1)
-    oldFrameGray = cv.cvtColor(oldFrame,cv.COLOR_BGR2GRAY)
+
+    oldFrame = cv.flip(oldFrame, 1)
+    oldFrameGray = cv.cvtColor(oldFrame, cv.COLOR_BGR2GRAY)
 
     plotter = Plotter(axes=dict(xtitle='x axis', ytitle='y axis', ztitle='z axis', yzGrid=False),
                         size=(oldFrame.shape[1], oldFrame.shape[0]),interactive=False)
     cv.namedWindow("Frame")
     cv.setMouseCallback("Frame", manageTrackedPoints)
-    vedo.show(faceMesh,axes=1)
+    vedo.show(faceMesh, axes=1)
 
-
-    """
     refDistance = 28.5 #cm
     refLength = 6. #cm
     refHeight = 7. #cm
     refDetectedLength = 145.4 #pixels
     refDetectedHeight = 161.3 #pixels
-    """
-
-    refDistance = 58 #cm
-    refLength = 5. #cm
-    refHeight = 9. #cm
-    refDetectedLength = 59.0 #pixels
-    refDetectedHeight = 107.0 #pixels
-
 
     refArea = refLength*refHeight
     refDetectedArea = refDetectedLength*refDetectedHeight
 
     focalLength = getFocalLength(refDistance, refArea, refDetectedArea)
-
 
     while True:
 
@@ -177,11 +149,11 @@ if __name__ == "__main__":
         if not success:
             raise ValueError("The system failed in reading the next frame.")
 
-        newFrame = cv.flip(newFrame,1)
-        newFrameGray = cv.cvtColor(newFrame,cv.COLOR_BGR2GRAY)
+        newFrame = cv.flip(newFrame, 1)
+        newFrameGray = cv.cvtColor(newFrame, cv.COLOR_BGR2GRAY)
 
         if pointSelected == True:
-            newPoints, status, error = cv.calcOpticalFlowPyrLK(oldFrameGray,newFrameGray,oldPoints,None,**lkParams)
+            newPoints, status, error = cv.calcOpticalFlowPyrLK(oldFrameGray, newFrameGray, oldPoints, None, **lkParams)
 
             for point in newPoints:
                 cv.circle(newFrame, (int(point[0]), int(point[1])), 5, (0, 255, 0), -1)
@@ -201,8 +173,7 @@ if __name__ == "__main__":
                     continue
 
                 moveFace(oldPoints, newPoints, focalLength, refArea)
-                rotateFace(oldRectangle,newRectangle)
-                printFacePosition(faceMesh)
+                faceMesh.rotateY(getRotationAngle(oldRectangle, newRectangle), locally=True)
 
             oldPoints = newPoints
             oldFrameGray = newFrameGray.copy()

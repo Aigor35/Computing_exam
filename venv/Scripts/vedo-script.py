5f15223cfff41ab108ef010ad952569593d53e0b
Aigor35 <alexcolavecchia1356@gmail.com>
2021-10-01 11:26:17 +0200

    I emptied the file gitignore, and added to git the libraries, the directories and the files required for the project. In this way it should be possible to directly download the project directory and immediately start using the program without the need to manually download all the libraries on PyCharm.

---
+++ venv/Scripts/vedo-script.py
@@ -0,0 +1,33 @@
+#!C:\Users\alexc\Desktop\Master_Degree\Computing\Computing_exam\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'vedo==2021.0.5','console_scripts','vedo'
+import re
+import sys
+
+# for compatibility with easy_install; see #2198
+__requires__ = 'vedo==2021.0.5'
+
+try:
+    from importlib.metadata import distribution
+except ImportError:
+    try:
+        from importlib_metadata import distribution
+    except ImportError:
+        from pkg_resources import load_entry_point
+
+
+def importlib_load_entry_point(spec, group, name):
+    dist_name, _, _ = spec.partition('==')
+    matches = (
+        entry_point
+        for entry_point in distribution(dist_name).entry_points
+        if entry_point.group == group and entry_point.name == name
+    )
+    return next(matches).load()
+
+
+globals().setdefault('load_entry_point', importlib_load_entry_point)
+
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(load_entry_point('vedo==2021.0.5', 'console_scripts', 'vedo')())
